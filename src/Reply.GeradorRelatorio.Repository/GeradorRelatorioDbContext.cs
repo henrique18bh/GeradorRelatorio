39a41c56300707fe365d43d121c3280dddfef97f
Henrique Burle Melgaço <henriquebm@ciandt.com>
2019-05-13 13:56:46 -0300

    Ajuste no projeto

--- src/Reply.GeradorRelatorio.Repository/GeradorRelatorioDbContext.cs
+++ src/Reply.GeradorRelatorio.Repository/GeradorRelatorioDbContext.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Reply.GeradorRelatorio.Repository
 {
-    public class GeradorRelatorioDbContext
+    public class GeradorRelatorioDbContext : DbContext
     {
+        public GeradorRelatorioDbContext() : base("GeradorRelatorioDbConnectionString")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<GeradorRelatorioDbContext>());
+
+            //Database.SetInitializer<SchoolDBContext>(new DropCreateDatabaseIfModelChanges<SchoolDBContext>());
+            //Database.SetInitializer<SchoolDBContext>(new DropCreateDatabaseAlways<SchoolDBContext>());
+            //Database.SetInitializer<SchoolDBContext>(new SchoolDBInitializer());
+        }
+        public DbSet<HistorioRelatorio> HistorioRelatorios { get; set; }
     }
 }
